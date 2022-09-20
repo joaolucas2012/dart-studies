16859b38440765a1401b3bf19c893b73680c5654
joaolucas2012 <ribeirojoaolucas68@gmail.com>
2022-09-20 14:25:00 -0300

    :book: learning about variables in Dart

---
+++ 1-variaveis-e-tipos.dart
@@ -0,0 +1,14 @@
+void main() {
+  // Variaveis
+  String nome = "João Lucas";
+  int idade = 21;
+  bool verdadeiro = true;
+  List<String> listaDePalavras = ["João Lucas", "Ribeiro"];
+
+  // Printar coisas
+  print(nome);
+  print(idade);
+  print(verdadeiro);
+  print(listaDePalavras);
+  print('${listaDePalavras[0]} - ${listaDePalavras[1]}');
+}
