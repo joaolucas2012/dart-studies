2c4e0f0dfde8ec2a04370ffef979495bc503b325
joaolucas2012 <ribeirojoaolucas68@gmail.com>
2022-09-20 14:53:38 -0300

    :book: learning about for and while loops in Dart

---
+++ 4-estruturas-de-repeticao.dart
@@ -0,0 +1,15 @@
+void main() {
+  // Estruturas de repetição
+
+  // **** FOR **** //
+  for (int i = 0; i <= 10; i++) {
+    print(2 * i);
+  }
+
+  // **** WHILE **** //
+  int contador = 10;
+  while (contador != -5) {
+    print('Loop -> ${contador}');
+    contador -= 1;
+  }
+}
