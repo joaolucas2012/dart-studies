0d983a0344ce6720e2aab19105ae89ca06908de0
joaolucas2012 <ribeirojoaolucas68@gmail.com>
2022-10-03 14:09:22 -0300

    [feat] add exercise 9

---
+++ 3-exercicios/9-calcula-saldo.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+void main() {
+  /* 
+    25) Faça um algoritmo para ler: número da conta do cliente, saldo, débito e crédito. Após, calcular e escrever o saldo atual (saldo atual = saldo - débito + crédito). Também testar se saldo atual for maior ou igual a zero escrever a mensagem 'Saldo Positivo', senão escrever a mensagem 'Saldo Negativo'. 
+  */
+  double numConta = askAndGetCorrespondingValue("Digite o numero da conta: ");
+  double saldo = askAndGetCorrespondingValue("Digite seu saldo: ");
+  double debito = askAndGetCorrespondingValue("Digite o débito: ");
+  double credito = askAndGetCorrespondingValue("Digite o crédito: ");
+
+  double saldoAtual = calcSaldoAtual(saldo, debito, credito);
+  print("\nSaldo atual: $saldoAtual\n");
+  printResult(numConta, verificaSaldoAtual(saldoAtual));
+}
+
+// funções
+double askAndGetCorrespondingValue(String frase) {
+  print("$frase");
+  return double.parse(stdin.readLineSync()!);
+}
+
+double calcSaldoAtual(double saldo, double debito, double credito) {
+  return saldo - debito + credito;
+}
+
+String verificaSaldoAtual(double saldoAtual) {
+  return saldoAtual >= 0 ? "Positivo" : "Negativo";
+}
+
+void printResult(double numConta, String saldoAtualVerificado) {
+  print("Conta: ${numConta.toInt()}\nSaldo $saldoAtualVerificado");
+}
