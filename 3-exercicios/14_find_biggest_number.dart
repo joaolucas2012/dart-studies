7025ce9af9aa76906b8b77007a44953a9f325bf5
joaolucas2012 <ribeirojoaolucas68@gmail.com>
2022-10-04 13:47:03 -0300

    [feat] add exercise 14

---
+++ 3-exercicios/14_find_biggest_number.dart
@@ -0,0 +1,22 @@
+import '0-funcao-global-leitura-de-valores.dart' as global;
+
+void main() {
+  /* 
+  19) Ler dois valores (considere que não serão lidos valores iguais) e escrever o maior deles. 
+  */
+  List<int> values = [];
+  for (int i = 0; i < 2; i++) {
+    values.add(global.askAndGetCorrespondingIntValue("Digite o valor $i:"));
+  }
+
+  printResult(findBiggestNumber(values));
+}
+
+// Funções
+int findBiggestNumber(List<int> numbers) {
+  return numbers[0] > numbers[1] ? numbers[0] : numbers[1];
+}
+
+void printResult(int biggestNumber) {
+  print("O maior valor digitado é: $biggestNumber");
+}
